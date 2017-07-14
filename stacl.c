41b05240deff1e27af6c77cb6927b9de5b91ce56
nooralam6 <shkhnooralam6@gmail.com>
2017-07-14 16:14:21 +0530

    First commit stack program

---
+++ stacl.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<stdlib.h>
+#define [MAX_SIZE] 10
+int stack[MAX_SIZE],top=-1;
+int isFull(){
+	if(MAX_SIZE==
+}
+int is Empty() {
+	return (top==MAX_SIZE-1?1:-1)
+}
+int peek(){
+
+	return stack[top];
+}
+
+void push(int e){
+	 
+	if(!isFull ())
+	{
+	}
+	else{
+}
+void pop(){
+	int d;
+	if(!isEmpty())
+	{
